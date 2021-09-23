a192945c0ede66b6c2bd0e188f908a4570c91dc4
royS <72430659+sohamroy19@users.noreply.github.com>
2021-09-23 20:41:55 +0530

    Matrix solution for Fibonacci

---
+++ interesting/Matrix.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+
+class Mat {
+public:
+    int n, m;
+    std::vector<std::vector<int>> a;
+
+    Mat() {}
+    Mat(int nInit, int mInit)
+        : n(nInit), m(mInit),
+          a(std::vector<std::vector<int>>(n, std::vector<int>(m))) {}
+    Mat(std::vector<std::vector<int>> &v)
+        : n(v.size()), m(n ? v[0].size() : 0), a(v) {}
+
+    inline void makeIdentity() { // logically should have n == m
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) a[i][j] = (i == j);
+    }
+    inline Mat operator+(const Mat &b) { // needs n == b.n && m == b.m
+        Mat ans = Mat(n, m);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                ans.a[i][j] = (a[i][j] + b.a[i][j]); // % MOD;
+        return ans;
+    }
+    inline Mat operator-(const Mat &b) { // needs n == b.n && m == b.m
+        Mat ans = Mat(n, m);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                ans.a[i][j] = (a[i][j] - b.a[i][j]); // + MOD) % MOD;
+        return ans;
+    }
+    inline Mat operator*(const Mat &b) { // needs m == b.n
+        Mat ans = Mat(n, b.m);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                for (int k = 0; k < b.m; k++)
+                    ans.a[i][k] += a[i][j] * b.a[j][k];
+        // ans.a[i][k] = (ans.a[i][k] + 1LL * a[i][j] * b.a[j][k] % MOD) % MOD;
+        return ans;
+    }
+    inline Mat &operator+=(const Mat &b) { return *this = (*this) + b; }
+    inline Mat &operator-=(const Mat &b) { return *this = (*this) - b; }
+    inline Mat &operator*=(const Mat &b) { return *this = (*this) * b; }
+    inline bool operator==(const Mat &b) { return a == b.a; }
+    inline bool operator!=(const Mat &b) { return a != b.a; }
+    inline Mat pow(long long k) { // needs n == m
+        Mat ans(n, n), t(a);
+        ans.makeIdentity();
+        while (k) {
+            if (k % 2 != 0) ans *= t;
+            t *= t;
+            k /= 2;
+        }
+        return ans;
+    }
+};
+
+int recurrer(std::vector<std::vector<int>> f, std::vector<std::vector<int>> i, int n) {
+    Mat func(f), init(i);
+    return (func.pow(n - 1) * init).a[1][0];
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::cout << "F(" << n << ") = " << recurrer({{1, 1}, {1, 0}}, {{1}, {1}}, n) << "\n";
+
+    return 0;
+}
