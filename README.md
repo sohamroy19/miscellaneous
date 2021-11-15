d6810ce4283aedb395f5802a4eeb21ccb0d56b37
royS <72430659+sohamroy19@users.noreply.github.com>
2021-11-16 04:42:16 +0530

    Added WhatsappStats.py

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 Compilation of interesting codes.
 - `SubsetsPrinter.c` generates all subsets of a set (naively)
-- `SubsetsPrinter.cpp` generates all subsets of a set (elegantly)
+- `SubsetsPrinter2.cpp` generates all subsets of a set (elegantly)
 - `Matrix.cpp` is a basic matrix implementation
+- `WhatsappStats.py` gives the total number of messages sent by every user
---
+++ WhatsapperStats.py
@@ -0,0 +1,34 @@
+import re
+
+# open the file
+f = open("chat.txt", encoding="utf8")
+
+# idk how to use map data structure
+senders = []
+counts = []
+total = 0
+
+# read line by line, stripping is always fun
+for x in f:
+    x = x.strip()
+
+    # check if line is the beginning of a message
+    m = re.match("\d+/\d+/\d+, \d+:\d+ - .+:", x)
+    if m:
+        sender = x.split("-")[1].split(":")[0].strip()  # extract sender
+        total += 1  # increment total
+
+        try:
+            i = senders.index(sender)  # check if sender already exists
+        except ValueError:  # if not, then add entry
+            senders.append(sender)
+            counts.append(1)
+        else:  # if yes, then increment count
+            counts[i] += 1
+
+# List of senders and counts
+for s, c in zip(senders, counts):
+    print(s, " : ", c)
+
+# Total
+print("Total : ", total)
