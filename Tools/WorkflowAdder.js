97636a309e53e2faf8cbd2f2b94e471aa1c15093
Soham Roy <72430659+sohamroy19@users.noreply.github.com>
2023-08-03 00:29:27 +0530

    Bugfix

--- Tools/WorkflowAdder.js
+++ Tools/WorkflowAdder.js
@@ -5,12 +5,16 @@
 // 4. Open the browser's Javascript console (Ctrl/Cmd + Shift + J or F12)
 // 5. Copy & paste the code below into the console, then press Enter
 
-do {
-    document.getElementById("ctl00_MainContent_btnNonCurAdd").click();
-    console.log(new Date().toLocaleTimeString() + " Clicked!");
-    await new Promise(resolve => setTimeout(resolve, 5000)); // 5 seconds
-} while (document.getElementById("ctl00_MainContent_lblErrorMsg").textContent === "There is no vacancy for this course");
+async function submit() {
+    do {
+        document.getElementById("ctl00_MainContent_btnNonCurAdd").click();
+        console.log(new Date().toLocaleTimeString() + " Clicked!");
+        await new Promise(resolve => setTimeout(resolve, 5000)); // 5 seconds
+    } while (document.getElementById("ctl00_MainContent_lblErrorMsg").textContent === "There is no vacancy for this course");
 
-document.getElementById("ctl00_MainContent_gvBasketCourses_ctl02_chkCell_CC").click();
-document.getElementById("ctl00_MainContent_btnSubmit").click();
-console.log(new Date().toLocaleTimeString() + " Submitted!");
+    document.getElementById("ctl00_MainContent_gvBasketCourses_ctl02_chkCell_CC").click();
+    document.getElementById("ctl00_MainContent_btnSubmit").click();
+    console.log(new Date().toLocaleTimeString() + " Submitted!");
+}
+
+submit();
